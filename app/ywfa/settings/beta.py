fe98fd430628d9220c7acfd463de3cbf615dbc45
Uniquode Team <davidn+uniquode@uniquode.io>
2020-08-27 11:47:28 +1000

    Deployment changes

--- app/ywfa/settings/beta.py
+++ app/ywfa/settings/beta.py
@@ -3,9 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!&_i25jj8^s)-2y86cm@0ecsjx#2v=2o7*abboqz!^ycf1!zho'
-
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
 
--- app/ywfa/settings/local.py
+++ app/ywfa/settings/local.py
@@ -4,13 +4,14 @@ These are essentially overrides to the default django/wagtail configuration
 Unfortunately this does not work for variables with dependencies in the
 main configuration code.
 """
-# import os
-#
+import os
+
 # PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # Override defaults
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', globals().get('SECRET_KEY'))
 
 try:
     from .secrets import *
