3fcefc7901a050a2544305fa60b592dd6a212d08
David Nugent <davidn@uniquode.io>
2020-08-28 09:59:40 +1000

    Switch to gunicorn instead of uvicorn as server to use django synchronous mode
    - hopefully temporary

--- startapp.sh
+++ startapp.sh
@@ -3,4 +3,5 @@ cd ${DJANGO_ROOT}
 npm install
 python manage.py migrate
 python manage.py collectstatic --no-input
-exec uvicorn ${APP_NAME}.asgi:application --host 0.0.0.0 --port 8000 --workers 3 --access-log --use-colors
+# exec uvnicorn ${APP_NAME}.wsgi:application --host 0.0.0.0 --port 8000 --workers 5 --access-log --use-colors
+exec gunicorn ${APP_NAME}.wsgi:application --bind 0.0.0.0:8000 --worker-connections 5 --access-logfile /dev/stdout --access-errorfile /dev/stdout
