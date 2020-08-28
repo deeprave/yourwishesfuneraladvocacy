1aa8cfd07496d68bfece32d786c151578868db05
David Nugent <davidn@uniquode.io>
2020-08-28 10:47:55 +1000

    fixes error on startup script

--- startapp.sh
+++ startapp.sh
@@ -4,4 +4,4 @@ npm install
 python manage.py migrate
 python manage.py collectstatic --no-input
 # exec uvnicorn ${APP_NAME}.wsgi:application --host 0.0.0.0 --port 8000 --workers 5 --access-log --use-colors
-exec gunicorn ${APP_NAME}.wsgi:application --bind 0.0.0.0:8000 --worker-connections 5 --access-logfile /dev/stdout --access-errorfile /dev/stdout
+exec gunicorn ${APP_NAME}.wsgi:application --bind 0.0.0.0:8000 --worker-connections 5 --access-logfile /dev/stdout --error-logfile /dev/stdout
