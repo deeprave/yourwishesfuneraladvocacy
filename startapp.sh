1396ea18a846f20b62454d8bebc66401f107d9f4
David Nugent <davidn@uniquode.io>
2020-08-29 11:03:25 +1000

    replace django-sass-processor with django-sass (#17)

--- startapp.sh
+++ startapp.sh
@@ -2,6 +2,7 @@
 cd ${DJANGO_ROOT}
 npm install
 python manage.py migrate
+python manage.py sass sass_src/ ywfa/static/scss
 python manage.py collectstatic --no-input
 # exec uvnicorn ${APP_NAME}.wsgi:application --host 0.0.0.0 --port 8000 --workers 5 --access-log --use-colors
 exec gunicorn ${APP_NAME}.wsgi:application --bind 0.0.0.0:8000 --worker-connections 5 --access-logfile /dev/stdout --error-logfile /dev/stdout
