539f1b4246ed27b7237cd32457eae0fd546a51ab
David Nugent <davidn@uniquode.io>
2020-09-06 23:53:03 +1000

    Make beta match production but don't send mail

--- app/ywfa/settings/beta.py
+++ app/ywfa/settings/beta.py
@@ -1,14 +1,10 @@
 from .base import *
+from .production import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+# identical to production, except the email backend
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/app-messages' # change this to a proper location
-
 
 try:
     from .local import *
