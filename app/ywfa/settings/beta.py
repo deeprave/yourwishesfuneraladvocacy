fd8c1e5490d982a1b63f76784337c9bb6b37d54c
David Nugent <davidn@uniquode.io>
2020-09-07 00:03:17 +1000

    bugfix: corrrect beta mode ALLOWED_HOSTS

--- app/ywfa/settings/beta.py
+++ app/ywfa/settings/beta.py
@@ -6,6 +6,15 @@ from .production import *
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/app-messages' # change this to a proper location
 
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '::1',
+    '172.105.169.83',
+    'beta.ywfa.com.au',
+    'beta.yourwishesfuneraladvocacy.com.au',
+]
+
 try:
     from .local import *
 except ImportError:
