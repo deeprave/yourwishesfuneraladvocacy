be3a8769d43b7fb06d39bc67d04939b164fcc7ac
Uniquode Team <davidn+uniquode@uniquode.io>
2021-01-11 03:09:40 +1100

    fix paths in s3 scripts

--- s3-download.sh
+++ s3-download.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 source ./.env
 cd media
-s3cmd -c ./.s3cfg sync "s3://${S3_API_BUCKET}" .
-
+s3cmd -c ../.s3cfg sync "s3://${S3_API_BUCKET}" .
--- s3-upload.sh
+++ s3-upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source .env || exit 1
 cd media
-find . -type f | sed -e 's/^\.\///' | xargs -I@ s3cmd put --acl-public --guess-mime-type "@" "s3://${S3_API_BUCKET}/@"
+find . -type f | sed -e 's/^\.\///' | xargs -I@ s3cmd -c ../.s3cfg put --acl-public --guess-mime-type "@" "s3://${S3_API_BUCKET}/@"
 
