ff89f004337c952c96755294ebffb5ebbcd27e80
Uniquode Team <davidn+uniquode@uniquode.io>
2021-01-11 03:12:01 +1100

    ignore and script update

--- ywfa-backup.sh
+++ ywfa-backup.sh
@@ -2,7 +2,7 @@ stamp=$(date +%Y%m%d_%H%M%S)
 name=ywfa-${stamp}
 pg_dump -f ${name}.sql -d -b -E utf-8 -c -h localhost -p 5432 -U postgres -d ywfa
 ls -l ${name}.sql
-grep -v '(SUBSCRIPTION|PUBLICATION)' < ${name}.sql > backup_${name}
+grep -v '(SUBSCRIPTION|PUBLICATION)' < ${name}.sql > backups/backup_${name}
 rm ${name}.sql
-bzip2 -9 backup_${name}
-ls -l backup_${name}*
+bzip2 -9 backups/backup_${name}
+ls -l backups/backup_${name}*
