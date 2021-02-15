9ef0a99edaa7ddb615695d9e5e9de37a819806c5
David Nugent <david.nugent@news.com.au>
2021-02-15 23:01:24 +1100

    Fix staticfiles path, fix gunicorn command line

--- startapp.sh
+++ startapp.sh
@@ -6,5 +6,5 @@ python manage.py migrate
 python manage.py sass sass_src/ static/scss/
 python manage.py collectstatic --no-input
 # exec uvnicorn ${APP_NAME}.wsgi:application --host 0.0.0.0 --port 8000 --workers 5 --access-log --use-colors
-exec gunicorn ${APP_NAME}.wsgi:application --bind 0.0.0.0:8000 --worker-connections 5  --disable_redirect_access_to_syslog --access-logfile - --error-logfile - --log-level debug
 #exec uwsgi --chdir ${DJANGO_ROOT} --wsgi-file ${APP_NAME}/wsgi.py uwsgi.ini
+exec gunicorn ${APP_NAME}.wsgi:application --bind 0.0.0.0:8000 --worker-connections 5 --access-logfile - --error-logfile - --log-level debug
