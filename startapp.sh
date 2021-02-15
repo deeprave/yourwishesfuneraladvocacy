dbddeda3a23960e56a1d76e3ffcb79ad2adabbc9
David Nugent <david.nugent@news.com.au>
2021-02-15 22:20:48 +1100

    MMore deployment fixes

--- startapp.sh
+++ startapp.sh
@@ -2,9 +2,9 @@
 cd ${DJANGO_ROOT}
 source .venv/bin/activate
 [ -f package.json ] && npm install
-python manage.py -q migrate
-python manage.py -q sass sass_src/
-python manage.py -q collectstatic --no-input
+python manage.py migrate
+python manage.py sass sass_src/ static/scss/
+python manage.py collectstatic --no-input
 # exec uvnicorn ${APP_NAME}.wsgi:application --host 0.0.0.0 --port 8000 --workers 5 --access-log --use-colors
-# exec uwsgi --chdir ${DJANGO_ROOT} --wsgi-file ${APP_NAME}/wsgi.py uwsgi.ini
 exec gunicorn ${APP_NAME}.wsgi:application --bind 0.0.0.0:8000 --worker-connections 5  --disable_redirect_access_to_syslog --access-logfile - --error-logfile - --log-level debug
+#exec uwsgi --chdir ${DJANGO_ROOT} --wsgi-file ${APP_NAME}/wsgi.py uwsgi.ini
