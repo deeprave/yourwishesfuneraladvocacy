feb7671e242ff54df5e2bc115feea7ba9429329b
David Nugent <david.nugent@news.com.au>
2021-02-16 14:05:27 +1100

    Minor corrections to scss/static file handling

--- startapp.sh
+++ startapp.sh
@@ -3,7 +3,7 @@ cd ${DJANGO_ROOT}
 source .venv/bin/activate
 [ -f package.json ] && npm install
 python manage.py migrate
-python manage.py sass sass_src/ static/scss/
+python manage.py sass sass_src/ cms/static/scss/
 python manage.py collectstatic --no-input
 # exec uvnicorn ${APP_NAME}.wsgi:application --host 0.0.0.0 --port 8000 --workers 5 --access-log --use-colors
 #exec uwsgi --chdir ${DJANGO_ROOT} --wsgi-file ${APP_NAME}/wsgi.py uwsgi.ini
