57636ce9e464a6d3e8b7dc404af0d910314c4a71
David Nugent <davidn@uniquode.io>
2021-03-15 22:45:54 +1000

    Push DJANGO_MODE into the env so manage.py picks it up correctly, skip .env

--- app/manage.py
+++ app/manage.py
@@ -4,9 +4,10 @@
 import sys
 from django_env import Env
 
+
 if __name__ == "__main__":
     env = Env()
-    env.setdefault("DJANGO_SETTINGS_MODULE", "ywfa.settings.environ." + env.get("DJANGO_MODE", "dev"))
+    env.setdefault("DJANGO_SETTINGS_MODULE", "ywfa.settings.environ." + env.get("DJANGO_MODE", "production"))
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
