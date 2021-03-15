c37c21923f93d82cf99dd9adf6f2fa6902ff2efe
David Nugent <davidn@uniquode.io>
2021-03-15 22:10:44 +1000

    Script fixes

--- scripts/upgrade-app.sh
+++ scripts/upgrade-app.sh
@@ -12,7 +12,7 @@ cmd () {
 cmd git fetch -atpf
 cmd git checkout master
 cmd git pull
-cmd ./make_env.py docker.env
+cmd scripts/sup.py build docker.env
 if [[ ! -f .latest || $(find . -mtime +24 -name .latest) ]]; then
    docker tag ${APP_NAME}:latest ${APP_NAME}:previous
    cmd touch .latest
